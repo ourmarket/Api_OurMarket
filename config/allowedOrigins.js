fe51d411d9b2cf30cd2fd7aaae26fafcd1bd5457
hernan066 <hernan0066@gmail.com>
2023-11-06 20:45:00 -0300

    Update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
 	'https://dashboard-ourmarket.netlify.app',
+	'https://delivery-ourmarket.netlify.app',
 	'http://127.0.0.1:5500',
 	'http://localhost:3500',
 	'http://localhost:3010',
